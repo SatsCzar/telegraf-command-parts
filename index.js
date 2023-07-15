17cd5e4b950dc45e02bae6db1da40d92052f77b8
SatsCzar <104809510+SatsCzar@users.noreply.github.com>
2023-07-15 20:12:19 -0300

    Use Composer.on

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const { Telegraf } = require('telegraf');
+const { Composer } = require('telegraf');
 
 const regex = /^\/([^@\s]+)@?(?:(\S+)|)\s?([\s\S]+)?$/i;
 
 /* eslint no-param-reassign: ["error", { "props": false }] */
-module.exports = () => Telegraf.mount('text', (ctx, next) => {
+module.exports = () => Composer.on('text', (ctx, next) => {
   const messageText = ctx.updateType === 'channel_post' ? ctx.channelPost.text : ctx.message.text;
   const parts = regex.exec(messageText);
   if (!parts) return next();
@@ -13,7 +13,7 @@ module.exports = () => Telegraf.mount('text', (ctx, next) => {
     bot: parts[2],
     args: parts[3],
     get splitArgs() {
-      return !parts[3] ? [] : parts[3].split(/\s+/).filter(arg => arg.length);
+      return !parts[3] ? [] : parts[3].split(/\s+/).filter((arg) => arg.length);
     },
   };
   ctx.state.command = command;
